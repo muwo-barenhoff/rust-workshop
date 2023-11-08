04a45e09bf16cb87f002e17aa9c77cc00e0a8f55
Markus Barenhoff <markus.barenhoff@multivac.de>
2023-11-08 06:50:19 +0100

    More Code

--- README.md
+++ README.md
@@ -16,15 +16,16 @@ Eine Installationsanleitung findet ihr [hier](https://doc.rust-lang.org/stable/b
 ## Ein paar Hintergründe
 
 * 2009 vom Mozilla Mitarbeiter Graydon Hoare entwickelt
-* Erster rustc compiler in rust 2011
+* Erster rustc compiler in rust 2011, vorher ocaml implementation
 * Mittlerweile 2021 Rust Foundation (u.a. Mozilla, AWS, Google et.al)
 * Mittlerweile sowohl auf Desktop, Server als auch Embedded eingesetzt
-* Projekte:
+* Bekannte Projekte die rust verwenden:
     * Mozilla Firefox
-    * Redox OS (Betriebssystem)
-    * Linux Kernel Support (support zum schreiben von Treibern in rust)
-    * Signal Messanger
-    * Redo
+    * [Redox OS](https://www.redox-os.org/) (Betriebssystem)
+    * [Linux Kernel Module in rust schreiben](https://github.com/Rust-for-Linux) (seit Linux 6.1 im offiziellem Kernel)
+    * [Signal Messanger](https://github.com/signalapp?language=rust)
+    * [Google fuchsia kernel](https://fuchsia.dev/fuchsia-src/development/languages/rust)
+* [crates.io](https://crates.io/) Bibliotheks repository mit mittlerweile > 130.000 Bibliotheken
 
 
 ## Link Sammlung
@@ -34,3 +35,4 @@ Eine Installationsanleitung findet ihr [hier](https://doc.rust-lang.org/stable/b
 * ["The Rust Reference"](https://doc.rust-lang.org/stable/reference/) - Referenz Handbuch zur Sprache
 * [RustDoc std](https://doc.rust-lang.org/std/) - Referenz zur Standard Bibliothek
 * [crates.io](https://crates.io/) - Bibliotheks Repository
+* ["The Embedded Rust Book"](https://docs.rust-embedded.org/book/) - Rust auf dem Mikrocontroller 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,174 @@
 fn main() {
     println!("Hello, world!");
 }
+
+//
+// Primitive Datentypen
+//
+
+fn _data_types_and_literals() {
+    // Integer Werte i8/u8/i16/u16/.../i128/u128
+    let _a = 8;
+    let _b = 9u8;
+    let _c: u8 = 0xff;
+    let _d = 65_5535;
+    let _e: u8 = 0b1111_0000;
+    let _f: i128 = - 23;
+
+
+
+    // Float Werte
+    let _ff1: f32 = 0.23;
+    let _ff2: f64 = 0.42;
+
+    // Bool
+    let _t = true;
+    let _f = false;
+
+    // char
+    let _ch2 = 'a';
+    let _ch2 = '😈';
+
+    // str
+    let _s = "foo bar";
+
+    // tuple
+    let _tup: (u8, bool, char) = (42u8, false, 'c');
+
+    // arrays
+    let _a1: [u64; 4] = [1, 2, 3, 4];
+}
+
+//
+// Funktionen
+//
+
+fn _f(_a: bool) -> () {}
+
+fn _inc_16(a: u32) -> u32 {
+    // explicit return
+    return a + 16;
+}
+
+fn _inc_16_or_32(s: bool, a: u32) -> u32 {
+    if s {
+        a + 16
+    } else {
+        a + 32
+    }
+    //a + if s { 16 } else { 32}
+}
+
+//
+// Loops
+//
+#[test]
+fn loop_test() {
+    let mut counter = 0;
+    let res = loop {
+        counter += 1;
+
+        if counter > 12 {
+            break counter * 2;
+        }
+    };
+    println!("Result is {res}");
+}
+
+//
+// structs / enums
+//
+
+pub struct S0;
+
+pub struct S1(u8, bool);
+
+pub struct S2 {
+    pub bool_value: bool,
+    pub num_value: i32,
+}
+
+pub enum E1 {
+    C1(u8),
+    C2(bool),
+    C3 { a: u8, b: u8 },
+}
+
+//
+// traits
+//
+
+trait MyEnumTrait {
+    fn print_enum(&self);
+}
+
+impl MyEnumTrait for E1 {
+    fn print_enum(&self) {
+        match self {
+            E1::C1(v) => {
+                println!("C1 with {v}");
+            }
+            E1::C2(v) => {
+                println!("C2 with {v}");
+            }
+            E1::C3 { a, b } => {
+                println!("C3 with {a} {b}");
+            }
+        }
+    }
+}
+
+#[test]
+pub fn _struct_enums() {
+    let _s0 = S0;
+    let _s1 = S1(42, false);
+    let _s2 = S2 {
+        bool_value: false,
+        num_value: 23,
+    };
+
+    let _e1 = E1::C1(2);
+    let _e2 = E1::C2(false);
+
+    match _e2 {
+        E1::C1(v) => {
+            println!("C1 with {v}");
+        }
+        E1::C2(v) => {
+            println!("C2 with {v}");
+        }
+        E1::C3 { a, b } => {
+            println!("C3 with {a} {b}");
+        }
+    }
+
+    _e1.print_enum();
+}
+
+#[test]
+fn test_fib() {
+    const N: usize = 23;
+    println!("{N}-th fibonacci number is {} {}", fib(N), fib_rec(N).1);
+}
+
+fn fib(n: usize) -> u128 {
+    let mut a = 0;
+    let mut b = 1;
+    for _i in 1..n {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    b
+}
+
+fn fib_rec(n: usize) -> (u128, u128) {
+    match n {
+        0 => (0, 0),
+        1 => (0, 1),
+        _ => {
+            let (a, b) = fib_rec(n - 1);
+            (b, a + b)
+        }
+    }
+}
